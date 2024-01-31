01043839584ad10d5ea7411875e935f5af654d3f
Kaspar Martin Suursalu <kasparsu@gmail.com>
2024-01-31 10:28:24 +0200

    Add pagination

--- src/pages/RickAndMorty.vue
+++ src/pages/RickAndMorty.vue
@@ -7,64 +7,15 @@
                 </button>
             </p>
 
-            <p v-for="num in 3" :key="num" class="control">
-                <button class="button">
+            <p v-for="num in makePagination(page, info.pages)" :key="num" class="control">
+                <button 
+                    class="button" 
+                    :class="{'is-static': num==='...', 'is-primary': num===page}"
+                    @click="getCharacters(num)">
                     <span>{{ num }}</span>
                 </button>
             </p>
 
-            <p class="control">
-                <button class="button is-static">
-                    <span>...</span>
-                </button>
-            </p>
-            
-            <p class="control ">
-                <button class="button">
-                    <span>{{ page-2 }}</span>
-                </button>
-            </p>
-            <p class="control ">
-                <button class="button">
-                    <span>{{ page-1 }}</span>
-                </button>
-            </p>
-            <p class="control ">
-                <button class="button is-primary">
-                    <span>{{ page }}</span>
-                </button>
-            </p>
-            <p class="control">
-                <button class="button">
-                    <span>{{ page+1 }}</span>
-                </button>
-            </p>
-            <p class="control">
-                <button class="button">
-                    <span>{{ page+2 }}</span>
-                </button>
-            </p>
-            <p class="control">
-                <button class="button is-static">
-                    <span>...</span>
-                </button>
-            </p>
-
-            <p class="control">
-                <button class="button">
-                    <span>{{ info.pages-2 }}</span>
-                </button>
-            </p>
-            <p class="control">
-                <button class="button">
-                    <span>{{ info.pages-1 }}</span>
-                </button>
-            </p>
-            <p class="control">
-                <button class="button">
-                    <span>{{ info.pages }}</span>
-                </button>
-            </p>
             <p class="control">
                 <button :disabled="!info.next" class="button" @click="next()">
                     <span>Next</span>
@@ -88,12 +39,13 @@ let chars = ref([]);
 let page = ref(1);
 getCharacters(page.value);
 
-async function getCharacters(page) {
+async function getCharacters(pageNumber) {
     let response = await axios.get('https://rickandmortyapi.com/api/character', {
         params: {
-            page // equal to page: page 
+            page: pageNumber
         }
     });
+    page.value=pageNumber;
     console.log(response.data);
     info.value = response.data.info;
     chars.value = response.data.results;
@@ -107,5 +59,21 @@ async function prev(){
     getCharacters(--page.value);
 }
 
-
+function makePagination(current, total){
+    let pages = [];
+    for(let i=1;i<=3;i++){
+        pages[i] = i;
+    }
+    if(current>2 && current<total-1){
+        pages.push('...');
+        for(let i=current-1;i<=current+1;i++){
+            pages[i] = i;
+        }
+    }
+    pages.push('...');
+    for(let i=total-2; i<=total;i++){
+        pages[i] = i;
+    }
+    return pages.filter(value => value);
+}
 </script>
