cd3e5b78382c9dca45e82303a526e03c23f41ca4
Kaspar Martin Suursalu <kasparsu@gmail.com>
2024-03-06 11:25:42 +0200

    Add boxen and chalk examples

--- bin/terminal.js
+++ bin/terminal.js
@@ -1,49 +1,11 @@
-/* eslint-disable no-undef */
+import chalk from 'chalk';
+import boxen from 'boxen';
 
-function write(text) {
-    process.stdout.write(text);
-}
+console.log(chalk.blue.bgGreen.underline('Hello world!'));
 
-import readline from 'node:readline';
-
-readline.emitKeypressEvents(process.stdin);
-console.clear();
-
-write('@');
-
-process.stdin.on('keypress', (ch, key) => {
-    //console.log('got "keypress"', ch, key);
-    if (key &&  key.name == 'q') {
-        //process.stdin.pause();
-        process.exit();
-    }
-
-    if (key &&  key.name == 'w') {
-        write('\x1B[1D');
-        write(' ');
-        write('\x1B[1D');
-        write('\x1B[1A');
-        write('@');
-    }
-    if (key &&  key.name == 's') {
-        write('\x1B[1D');
-        write(' ');
-        write('\x1B[1D');
-        write('\x1B[1B');
-        write('@');
-    }
-    if (key && key.name == 'a') {
-        write('\x1B[2D');
-        write('@');
-        write(' ');
-        write('\x1B[1D');
-    }
-    if (key && key.name == 'd') {
-        write('\x1B[1D');
-        write(' ');
-        write('@');
-    }
-
-});
-
-process.stdin.setRawMode(true);
+console.log(boxen(chalk.blue.bgGreen.underline('unicorn'), {
+    padding: 1,
+    margin: 2,
+    borderStyle: 'double',
+    title: chalk.blue.bgGreen.underline('Hello')
+}));
