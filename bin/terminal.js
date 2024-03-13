6b3300599f4c9af4a58f9d4291b87f208f5e833e
Kaspar Martin Suursalu <kasparsu@gmail.com>
2024-03-13 10:58:06 +0200

    Add comic scraper

--- bin/terminal.js
+++ bin/terminal.js
@@ -1,28 +1,31 @@
-import inquirer from 'inquirer';
+import axios from 'axios';
+import * as cheerio from 'cheerio';
+import fs from 'node:fs';
+const sleep = (delay) => new Promise((resolve) => setTimeout(resolve, delay));
 
-inquirer
-    .prompt([
-        { type: 'input', name: 'name', message: 'Whats your name?' },
-        { 
-            type: 'list',
-            name: 'food',
-            message: 'Whats your favorite food?',
-            choices: ['Pizza', 'Burger', 'Fries']
-        },
-        { 
-            type: 'checkbox',
-            name: 'foods',
-            message: 'What are your favorite foods?',
-            choices: ['Pizza', 'Burger', 'Fries']
-        },
-    ])
-    .then((answers) => {
-        console.log(answers);
-    })
-    .catch((error) => {
-        if (error.isTtyError) {
-            // Prompt couldn't be rendered in the current environment
-        } else {
-            // Something else went wrong
-        }
-    });
+if(!fs.existsSync('cache')){
+    fs.mkdirSync('cache');
+}
+
+
+for(let i = 2905; i>2895; i--){
+   
+    let cacheName = `cache/${i}.html`;
+    let data;
+    if(!fs.existsSync(cacheName)){
+        await sleep(1000);
+        let res = await axios.get(`https://xkcd.com/${i}/`);
+        data = res.data;
+        fs.writeFileSync(cacheName, data);
+        console.log('LIVE REQUEST!!!!');
+    } else {
+        data = fs.readFileSync(cacheName);
+    }
+    //console.log(res.data);
+    const $ = cheerio.load(data);
+    let img = $('div#comic>img');
+    console.log(img.attr('src'));
+    console.log(img.attr('title'));
+    console.log(img.attr('alt'));
+    
+}
